0c97d1639bcefd0c5dc9868e179ae4d2e542b19e
Paris Stefanou <gaspar093@hotmail.com>
2014-03-21 23:21:22 +0200

    no message

--- akgl/Units/GLTypes/Extensions/UI/Controls/textfields/NamedTextField.java
+++ akgl/Units/GLTypes/Extensions/UI/Controls/textfields/NamedTextField.java
@@ -1,12 +1,17 @@
 package akgl.Units.GLTypes.Extensions.UI.Controls.textfields;
 
 import akgl.Units.GLTypes.Extensions.UI.Controls.TextField;
+import akgl.Units.GLTypes.Extensions.UI.Text.TextLabel;
+import akgl.Units.GLTypes.GLObject;
 import akgl.Units.Geometry.Vectors.Vec2;
+import org.w3c.dom.Node;
 
 /**
  * @author Robert Kollar
  */
-public class PasswordTextfield extends TextField {
+public class NamedTextField extends TextField {
+
+    String namedtextfield = new String();
 
     @Override
     public void setText(String text) {
@@ -21,4 +26,32 @@ public class PasswordTextfield extends TextField {
         slicedSprite.forceBuild();
     }
 
+    @Override
+    protected void onExtensionAdded() {
+        super.onExtensionAdded(); //To change body of generated methods, choose Tools | Templates.
+        textLabel = new TextLabel();
+        textLabel.setText(namedtextfield);
+        GLObject textLabelHolder = new GLObject(getgLObject());
+        textLabelHolder.addExtension(textLabel);
+        textLabel.getScale().setX(0.75f);
+        textLabel.getScale().setY(0.75f);
+        textLabel.getTransform().getLocalPosition().setZ(1);
+        textLabel.setAnchor(UIAnchor.Center);
+    }
+
+    public String getName() {
+        return namedtextfield;
+    }
+
+    public void setName(String newname) {
+        namedtextfield = newname;
+    }
+
+    @Override
+    public void loadFromXML(Node node) {
+        super.loadFromXML(node);
+        if(node.getAttributes().getNamedItem(namedtextfield)!=null);
+            node.getAttributes().getNamedItem(namedtextfield);
+    }
+    
 }
--- akgl/Units/GLTypes/Extensions/UI/Controls/textfields/PasswordTextfield1.java
+++ akgl/Units/GLTypes/Extensions/UI/Controls/textfields/PasswordTextfield1.java
@@ -1,13 +1,15 @@
 package akgl.Units.GLTypes.Extensions.UI.Controls.textfields;
 
 import akgl.Units.GLTypes.Extensions.UI.Controls.TextField;
+import akgl.Units.GLTypes.Extensions.UI.Text.TextLabel;
+import akgl.Units.GLTypes.GLObject;
 import akgl.Units.Geometry.Vectors.Vec2;
 
 /**
  * @author Robert Kollar
  */
-public class PasswordTextfield extends TextField {
-
+public class PasswordTextfield1 extends TextField {
+    
     @Override
     public void setText(String text) {
         this.text = text;
@@ -20,5 +22,17 @@ public class PasswordTextfield extends TextField {
 
         slicedSprite.forceBuild();
     }
-
+        @Override
+    protected void onExtensionAdded() {
+        super.onExtensionAdded(); //To change body of generated methods, choose Tools | Templates.
+        textLabel = new TextLabel();
+        textLabel.setText("Password");
+        GLObject textLabelHolder = new GLObject(getgLObject());
+        textLabelHolder.addExtension(textLabel);
+        textLabel.getScale().setX(0.75f);
+        textLabel.getScale().setY(0.75f);
+        textLabel.getTransform().getLocalPosition().setZ(1);
+        textLabel.setAnchor(UIAnchor.Center);
+    }
 }
+
